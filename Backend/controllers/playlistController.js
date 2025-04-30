8246c7c8c140a23c70e0b0880f35511658b22661
pray <prayvijaybhai10@gmail.com>
2025-04-30 15:28:14 +0530

    playlist..

--- Backend/controllers/playlistController.js
+++ Backend/controllers/playlistController.js
@@ -1,6 +1,6 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
-import Playlistdata from "../models/playlist.js"
+import Playlistdata from "../models/playlist.js"    
 
 const addPlaylist = async(req , res) => {
     try {
