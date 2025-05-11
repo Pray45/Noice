0d5581019c52090ddd2963dee922e84f8c708f93
pray <prayvijaybhai10@gmail.com>
2025-05-11 18:50:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,10 @@ import userRouter from './routes/userRouter.js';
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
